4abcc5f278d7fcb68716c7433023b8ea29d8480e
Reini <118827636+BagelReini@users.noreply.github.com>
2023-02-23 10:40:10 +0100

    Add files via upload

---
+++ Debug/Black_tiled_but_different.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Black_tiled_but_different" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="Black_tiled_but_different.png" width="320" height="320"/>
+</tileset>
